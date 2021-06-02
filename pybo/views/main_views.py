04df08c8f1823d303af2f6b53ab9fff92fbe8ca4
suminwooo <wsm263@gmail.com>
2021-06-02 23:48:56 +0900

    2021.06.02 추가

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, render_template
-
-from pybo.models import Question
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 # 블루 프린트 객체 생성
 # 블루 프린트를 이용하면 라우트 함수를 구조적으로 관리할 수 있다.
@@ -11,7 +10,7 @@ def hello_pybo():
     return 'Hello, pybo!'
 
 @bp.route('/')
-def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('question/question_list.html',
-                            question_list=question_list)
+def index(): 
+    # question._list에 해당하는 URL로 리다이렉트할 수 있도록 수정
+    # redirect 함수는 입력받은 URL로 리다이렉트해 주고, url_for함수는 라우트가 설정된 함수명으로 URL을 역으로 찾아준다.
+    return redirect(url_for('question._list')) # question은 등록된 블루 프린트 이름, _list는 블루 프린트에 등록된 함수명
