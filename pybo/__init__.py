826077a1fcddc3c93e54e654922dac9eda82bfca
suminwooo <wsm263@gmail.com>
2021-06-17 23:17:08 +0900

    2020.06.17

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -16,9 +16,10 @@ naming_convention = {
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
-def create_app(): # 플라스크 내부에서 정의된 함수명
-    app = Flask(__name__) # 플라스크 애플리케이션을 생성하는 코드, 이코드에서 __name__이라는 변수에는 모듈명이 담긴다.
-    app.config.from_object(config) # app.config 환경 변수로 부르기 위해 추가
+
+def create_app():  # 플라스크 내부에서 정의된 함수명
+    app = Flask(__name__)  # 플라스크 애플리케이션을 생성하는 코드, 이코드에서 __name__이라는 변수에는 모듈명이 담긴다.
+    app.config.from_object(config)  # app.config 환경 변수로 부르기 위해 추가
 
     # ORM : object relational mapping : 데이터 베이스에 데이터를 저장하는 테이블을 파이ㅏ썬 클래스로 만들어 관리하는 기술
     db.init_app(app)
@@ -27,14 +28,16 @@ def create_app(): # 플라스크 내부에서 정의된 함수명
     else:
         migrate.init_app(app, db)
 
-    from . import models
+    # from . import models
 
     # 블루 프린트
-    from .views import main_views, question_views, answer_views, auth_views
-    app.register_blueprint(main_views.bp) # 블루프린트 객체 bp 등록 
+    from .views import main_views, question_views, answer_views, auth_views, comment_views, vote_views
+    app.register_blueprint(main_views.bp)  # 블루프린트 객체 bp 등록 
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
     app.register_blueprint(auth_views.bp)
+    app.register_blueprint(comment_views.bp)
+    app.register_blueprint(vote_views.bp)
 
     # 필터
     from .filter import format_datetime
--- pybo/models.py
+++ pybo/models.py
@@ -1,6 +1,17 @@
 # from sqlalchemy.orm import backref
 from pybo import db
 
+question_voter = db.Table(
+    'question_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('question_id', db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'), primary_key=True)
+)
+
+answer_voter = db.Table(
+    'answer_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('answer_id', db.Integer, db.ForeignKey('answer.id', ondelete='CASCADE'), primary_key=True)
+)
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +22,7 @@ class Question(db.Model):
         'user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('question_set'))
     modify_date = db.Column(db.DateTime(), nullable=True)
+    voter = db.relationship('User', secondary=question_voter, backref=db.backref('question_voter_set'))
 
 
 class Answer(db.Model):     
@@ -23,6 +35,7 @@ class Answer(db.Model):
         'user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('answer_set'))
     modify_date = db.Column(db.DateTime(), nullable=True)
+    voter = db.relationship('User', secondary=answer_voter, backref=db.backref('answer_voter_set'))
 
 
 class User(db.Model):
@@ -30,3 +43,15 @@ class User(db.Model):
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('comment_set'))
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    modify_date = db.Column(db.DateTime())
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'), nullable=True)
+    question = db.relationship('Question', backref=db.backref('comment_set'))
+    answer_id = db.Column(db.Integer, db.ForeignKey('answer.id', ondelete='CASCADE'), nullable=True)
+    answer = db.relationship('Answer', backref=db.backref('comment_set'))
